b9e74170c9b5d9db2075a3ce9f849e941cc62927
sankalp047 <38282659+sankalp047@users.noreply.github.com>
2022-04-06 16:29:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <html>
     <body>
+        <h2>Sankalp Sandeep Singh</h2>
+            <h2>1001964065</h2>
         <form method = 'post' action='page2.php'>
         <p> Enter data : <input name="user_string" type="text"/></p>
             <p><input type='submit' value='submit' name='submit'/></p>
